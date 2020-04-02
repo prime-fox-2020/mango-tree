b351f397e74d1b927fc1aec2efa232a7ca709495
rezarr-dev <rezarr.business@gmail.com>
2020-04-02 23:38:12 +0700

    release 2

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 "use strict"
 
-const matureAge = 3
-const maxGrowth = 10
+// Release 2
 
-// Release 0
-
-class MangoTree {
-
-  // Initialize a new MangoTree
+class FruitTree {
   constructor (age = 0, height = 0, fruits = 0, healthStatus = true, harvested = null) {
     this._age = age
     this._height = height
@@ -49,23 +44,39 @@ class MangoTree {
   }
   set harvested (param){
     this._harvested = param
-  }
+  }  
+}
 
-  // Get current states here
+
+class Fruit {
+
+}
+
+// Release 0
+
+const matureMango = 3
+const maxGrowthMango = 10
+
+class MangoTree extends FruitTree {
+
+  // Initialize a new MangoTree
+  constructor (age, height, fruits, healthStatus, harvested) {
+    super (age, height, fruits, healthStatus, harvested)
+  }
 
   // Grow the tree
   
   grow () {
     this.age += 1
     this.height += Math.round(Math.random()*10)/10
-    if (this.age > maxGrowth){
+    if (this.age >= maxGrowthMango){
       this.healthStatus = false
     }
   }
 
   // Produce some mangoes
   produceMangoes () {
-    if (this.age >= matureAge){
+    if (this.age >= matureMango){
       this.fruits = Math.round(Math.random()*10)
     }
   }
@@ -110,50 +121,15 @@ let mangoTree = new MangoTree()
  console.log(`\n=============================================================\n`)
 
  // Release 1
+
  const matureAgeApple = 5
  const maxGrowthApple = 15
-class AppleTree {
-  constructor (age = 0, height = 0, fruits = 0, healthStatus = true, harvested = null) {
-    this._age = age
-    this._height = height
-    this._fruits = fruits
-    this._healthStatus = healthStatus
-    this._harvested = harvested
-  }
 
-  get age () {
-    return this._age
-  }
-  set age (param) {
-    this._age = param
-  }
-
-  get height () {
-    return this._height
-  }
-  set height (param){
-    this._height = param 
-  }
-
-  get fruits () {
-    return this._fruits
-  }
-  set fruits (param){
-    this._fruits = param
-  }
-
-  get healthStatus () {
-    return this._healthStatus
-  }
-  set healthStatus (param){
-  this._healthStatus = param    
-  }
-
-  get harvested () {
-    return this._harvested
-  }
-  set harvested (param){
-    this._harvested = param
+ class AppleTree extends FruitTree {
+  
+  // Initialize a new AppleTree
+  constructor (age, height, fruits, healthStatus, harvested) {
+    super (age, height, fruits, healthStatus, harvested)
   }
 
   // Grow the tree
@@ -161,7 +137,7 @@ class AppleTree {
   grow () {
     this.age += 1
     this.height += Math.round(Math.random()*10)/10
-    if (this.age > maxGrowthApple){
+    if (this.age >= maxGrowthApple){
       this.healthStatus = false
     }
   }
@@ -208,7 +184,3 @@ do {
 
 console.log(`The tree has met its end. :sad:`)
 console.log(`\n=============================================================\n`)
-
-// Release 2
-// class FruitTree {}
-// class Fruit {}
