cf1470e278c3e462519754e01b3671c834cbb0cd
Eko Prasetyo <ekoprasetyo.me@gmail.com>
2020-04-02 20:05:29 +0700

    Release 0

--- index.js
+++ index.js
@@ -6,13 +6,20 @@ class MangoTree {
 
   // Initialize a new MangoTree
   constructor () {
+    this._age = 0,
+    this._height = 0,
+    this._isMature = false,
+    this._canGrow = true,
+    this._healthStatus = true,
+    this._harvested = 0
   }
 
   get age () {
+    return this._age;
   }
 
   get height () {
-
+    return `${this._height.toFixed(1)} m`;
   }
 
   get fruits () {
@@ -20,21 +27,37 @@ class MangoTree {
   }
 
   get healthStatus () {
-  
+    return this._healthStatus;
   }
 
   get harvested () {
-
+    return this._harvested;
   }
 
   // Get current states here
 
   // Grow the tree
   grow () {
+    if (this._canGrow) this._height += Math.random();
+
+    // Tambah umur
+    this._age++;
+
+    // Layak berbuah di umur ke 3
+    if (this._age === 3) this._isMature = true;
+
+    // Stop grow height
+    if (this._age === 8) this._canGrow = false;
+    
+    // Sudah saatnya pohon mati
+    if (this._age === 20) this._healthStatus = false;
   }
 
   // Produce some mangoes
   produceMangoes () {
+    if (this._isMature) {
+      this._harvested += Math.round(Math.random() * 15 * this._height);
+    }
   }
 
   // Get some fruits
@@ -61,6 +84,14 @@ class Mango {
   *   console.log(`[Year ${mangoTree.age} Report] Height = ${mangoTree.height} | Fruits harvested = ${mangoTree.harvested}`)
   * } while (mangoTree.healthStatus != false)
   */
+
+let mangoTree = new MangoTree()
+do {
+  mangoTree.grow();
+  mangoTree.produceMangoes();
+  mangoTree.harvest();
+  console.log(`[Year ${mangoTree.age} Report] Height = ${mangoTree.height} | Fruits harvested = ${mangoTree.harvested}`)
+} while (mangoTree.healthStatus != false)
 
 // Release 1
 class AppleTree {}
