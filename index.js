372e5f5d23f2d969c8466ee3b35618e802d6b6a7
Adib Faqih <foezst@gmail.com>
2020-04-02 23:12:17 +0700

    release 2, create parent object

--- index.js
+++ index.js
@@ -188,7 +188,7 @@ class AppleTree {
   produceApples () {
     let rand = Math.random() * 50;
     for (let i = 0; i < rand; i++) {
-      this.fruits.push(new Mango);
+      this.fruits.push(new Apple);
     }
     
     for (let i = 0; i < this.fruits.length; i++) {
@@ -232,5 +232,167 @@ class Apple {
 // } while (appleTree.healthStatus != false)
 
 // Release 2
-class FruitTree {}
-class Fruit {}
+class FruitTree {
+  // Initialize a new FruitTree
+  constructor () {
+    this._age = 0; //umur pohon
+    this._height = 0; //tinggi pohon
+    this._fruits = []; //jumlah buah
+    this._healthStatus = 12; //umur maksimal pohon
+    this._harvested = false; //keterangan berapa buah yang dipanen
+    this._maxHeight = 200; //tinggi maksimal
+    this._maxFruit = 50; //jumlah maksimal buah
+    this.matureAge = 3; //usia pohon berbuah
+    this.fruit = Fruit; //pilih objek buah
+    this.increase = 80; //maksimal pertumbuhan pertahun
+  }
+
+  get age () {
+    return this._age;
+  }
+
+  get height () {
+    return this._height;
+  }
+
+  get fruits () {
+    return this._fruits;
+  }
+
+  get healthStatus () {
+    return this._healthStatus;
+  }
+
+  get harvested () {
+    return this._harvested;
+  }
+
+  set age (par) {
+    this._age = par;
+  }
+
+  set height (par) {
+    this._height = par;
+  }
+
+  set fruits (par) {
+    this._fruits = par;
+  }
+
+  set healthStatus (par) {
+    this._healthStatus = par;
+  }
+
+  set harvested (par) {
+    this._harvested = par;
+  }
+
+  // Get current states here
+
+  // Grow the tree
+  grow () {
+    this.age++;
+    let plus = Math.random()*this.increase;
+    this.height += parseInt(plus);
+    if (this.height >= this._maxHeight) {
+      this.height = this._maxHeight;
+    }
+    
+    this.healthStatus--;
+  }
+
+  // Produce some fruits
+  produceFruits () {
+    let rand = Math.random() * this._maxFruit;
+    for (let i = 0; i < rand; i++) {
+      this.fruits.push(new this.fruit);
+    }
+
+    for (let i = 0; i < this.fruits.length; i++) {
+      if (Math.random() > 0.5) {
+        this.fruits[i].good++;
+      } else {
+        this.fruits[i].bad++;
+      }
+    }
+    if (this.age < this.matureAge) this.fruits = [];
+  }
+  
+  // Get some fruits
+  harvest () {
+    let good = 0, bad = 0;
+    for (let i = 0; i < this.fruits.length; i++) {
+      if (this.fruits[i].good) {
+        good++;
+      } else {
+        bad++;
+      }
+    }
+    this.harvested = `${this.fruits.length} (${good} good, ${bad} bad)`
+    this.fruits = [];
+  }
+
+}
+
+class Fruit {
+  // Produce a fruit
+  constructor () {
+    this.bad = 0;
+    this.good = 0;
+  }
+}
+
+class MangoTree1 extends FruitTree{
+  constructor () {
+    super();
+    this._healthStatus = 20;
+    this._maxHeight = 500;
+    this._maxFruit = 80;
+    this.matureAge = 5;
+    this.fruit = Mango1;
+  }
+}
+
+class Mango1 extends Fruit{
+  constructor () {
+    super();
+  }
+}
+
+class AppleTree1 extends FruitTree{
+  constructor () {
+    super();
+    this._healthStatus = 14;
+    this._maxHeight = 260;
+    this._maxFruit = 100;
+    this.fruit = Apple1;
+    this.increase = 50;
+  }
+}
+
+class Apple1 extends Fruit{
+  constructor () {
+    super();
+  }
+}
+
+//example
+/*
+console.log(new Apple1)
+let mangoTree = new MangoTree1();
+do {
+  mangoTree.grow();
+  mangoTree.produceFruits();
+  mangoTree.harvest();
+  console.log(`[Year ${mangoTree.age} Report] Height = ${mangoTree.height} cm | Fruits harvested = ${mangoTree.harvested}`)
+} while (mangoTree.healthStatus != false)
+console.log('')
+
+let appleTree = new AppleTree1();
+do {
+  appleTree.grow();
+  appleTree.produceFruits();
+  appleTree.harvest();
+  console.log(`[Year ${appleTree.age} Report] Height = ${appleTree.height} cm | Fruits harvested = ${appleTree.harvested}`)
+} while (appleTree.healthStatus != false)
+*/
