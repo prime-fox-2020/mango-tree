81248b30ec051fec717489ae00be0a47243391fb
Mercen1 <nurhabib102@gmail.com>
2020-04-02 20:50:34 +0700

    Release1 : AppleTree and Aplle Classes

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ class MangoTree {
     //Pohon Mati Umur 21Tahun
     if(this.age>20){
       this._healthStatus = false
-      console.log('The tree has met its end. :sad:')
+      console.log('The Mango tree has met its end. :sad:')
     }
 
     this._height = Math.floor(this._height*10)/10 ;
@@ -106,7 +106,7 @@ class Mango {
 // driver code untuk release 0
 let mangoTree = new MangoTree()
 mangoTree.harvest()
-console.log('The tree is alive! :smile:')
+console.log('The Mango tree is alive! :smile:')
 do {
   if(mangoTree.age>0){
   console.log(`[Year ${mangoTree.age} Report] Height = ${mangoTree.height} m | Fruits harvested = ${mangoTree.harvested}`);
@@ -117,10 +117,126 @@ do {
 } while (mangoTree.healthStatus != false)
 
   
-
+console.log(`\n ================================================================= `)
+console.log(` ================================================================= \n`)
 // // Release 1
 // class AppleTree {}
 // class Apple {}
+class AppleTree {
+
+  // Initialize a new AppleTree
+  constructor () {
+    this._age = 0 
+    this._height = 0
+    this._fruits = 0 
+    this._healthStatus = true
+    this._harvested = []
+  }
+
+  get age () {
+    return this._age
+  }
+
+  get height () {
+    return this._height
+  }
+
+  get fruits () {
+    return this._fruits
+  }
+
+  get healthStatus () {
+    return this._healthStatus
+  }
+
+  get harvested () {
+    return this._harvested
+  }
+
+  // Get current states here
+
+  // Grow the tree
+  grow () {
+    this._age ++
+    if (this._age == 1){
+      this._height = 0.4
+    }else if(this._age>1 && this._age <=10){
+      this._height += Math.random()*0.5
+    }else if(this._age<=25){
+      this._height += Math.random()*0.25
+    }
+    //Pohon Mati Umur 21Tahun
+    if(this.age>30){
+      this._healthStatus = false
+      console.log('The Aplle tree has met its end. :sad:')
+    }
+
+    this._height = Math.ceil(this._height*10)/10 ;
+    
+    return this
+  }
+
+  // Produce some mangoes
+  // Aplle Berbuah biasanya Umur 5 tahun
+  produceMangoes () {
+    if(this._age>=5 && this._age<=10){
+      this._fruits = Math.random()*30
+    }else if(this.age>8){
+      this._fruits = Math.random()*50
+    }
+  }
+  
+
+  // Get some fruits
+  harvest () {
+  
+    let good =0;
+    let bad = 0
+    for (let i = 0; i < this.fruits; i++) {
+      let tesApple = new Apple();
+      tesApple.cek()
+      if(tesApple.quality == 'good'){
+        good ++
+      }else{
+        bad ++
+      }
+    }
+    this._harvested=`${good+bad} (${good} good, ${bad} bad)`
+  }
+
+}
+
+class Apple {
+  // Produce a mango
+  constructor () {
+    this.quality =''
+  }
+
+  cek(){
+    let angka = Math.random();
+    if(angka<0.2){
+      this.quality = 'bad'
+    }else{
+      this.quality = 'good'
+    }
+  }
+
+}
+
+// driver code untuk release 0
+let appleTree = new AppleTree()
+appleTree.harvest()
+console.log('The Aplle tree is alive! :smile:')
+do {
+  if(appleTree.age>0){
+  console.log(`[Year ${appleTree.age} Report] Height = ${appleTree.height} m | Fruits harvested = ${appleTree.harvested}`);
+  }
+  appleTree.grow();
+  appleTree.produceMangoes();
+  appleTree.harvest();
+} while (appleTree.healthStatus != false)
+
+
 
 // // Release 2
 // class FruitTree {}
